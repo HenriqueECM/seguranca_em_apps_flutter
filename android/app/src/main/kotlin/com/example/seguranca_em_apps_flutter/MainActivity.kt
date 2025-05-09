e67da1a4d28388b18b2654f45b5359ae12946779
henrique <henriqueworks68@gmail.com>
2025-05-08 21:38:50 -0300

    First commit

---
+++ android/app/src/main/kotlin/com/example/seguranca_em_apps_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.seguranca_em_apps_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 255, 255, 255)),
+      ),
+      home: const segurancaApp(),
+    );
+  }
+}
+
+class segurancaApp extends StatefulWidget {
+  const segurancaApp({super.key});
+
+  @override
+  State<segurancaApp> createState() => _segurancaAppState();
+}
+
+class _segurancaAppState extends State<segurancaApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('📘 Segurança em Apps Flutter'),),
+      body: Padding(padding: const EdgeInsets.all(16.0),
+      child: Column(
+        children: [
+          TextFormField(
+            decoration: const InputDecoration(
+              labelText: 'Email *',
+              border: OutlineInputBorder(), // borda retangular
+            ),
+            validator: (String? value) {
+              return (value == null || !value.contains('@')) ? 'O campo deve conter o caractere @.' : null;
+            },
+          ),
+
+          const SizedBox(height: 24),
+
+          TextFormField(
+            decoration: const InputDecoration(
+              labelText: 'Senha *',
+              border: OutlineInputBorder(), // borda retangular
+            ),
+            validator: (String? value) {
+              return (value == null || !value.contains('@')) ? 'O campo deve conter o caractere @.' : null;
+            },
+          )
+        ],
+      ),
+      ),
+    );
+  }
+}
